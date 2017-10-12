43bcd9a4b380de8ee67bd0d93f6486d0844b2036
Debugg3R <debugg3r@gmail.com>
2017-10-12 23:33:00 +0300

    Loading progress

---
+++ app/src/main/java/com/debugg3r/android/solarwallpaper/model/FileService.java
@@ -0,0 +1,68 @@
+package com.debugg3r.android.solarwallpaper.model;
+
+import android.net.Uri;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class FileService {
+
+    private static final String LOG_TAG = "FILE_SERVICE";
+
+    public static File getOutputMediaFile(){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "MyCameraApp");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_"+ timeStamp + ".jpg");
+
+        return mediaFile;
+    }
+
+    public static void onPictureTaken(byte[] data) {
+
+        File pictureFile = getOutputMediaFile();
+        if (pictureFile == null){
+            Log.d(LOG_TAG, "Error creating media file, check storage permissions: " +
+//                        e.getMessage());
+                    "");
+            return;
+        }
+
+        try {
+            FileOutputStream fos = new FileOutputStream(pictureFile);
+            fos.write(data);
+            fos.close();
+
+            Uri uri = Uri.fromFile(pictureFile);
+
+        } catch (FileNotFoundException e) {
+            Log.d(LOG_TAG, "File not found: " + e.getMessage());
+        } catch (IOException e) {
+            Log.d(LOG_TAG, "Error accessing file: " + e.getMessage());
+        }
+    }
+
+}
--- app/src/main/java/com/debugg3r/android/solarwallpaper/presenter/MainPresenterImpl.java
+++ app/src/main/java/com/debugg3r/android/solarwallpaper/presenter/MainPresenterImpl.java
@@ -19,7 +19,6 @@ public class MainPresenterImpl implements MainPresenter {
     private static final String LOG_TAG = "MAIN_PRESENTER";
     private DataManager mDataManager;
     private MainView mView;
-    private MainView mDummyView;
 
     private MainView getView() {
         return mView;
@@ -27,45 +26,11 @@ public class MainPresenterImpl implements MainPresenter {
 
     public MainPresenterImpl(DataManager mDataManager) {
         this.mDataManager = mDataManager;
-
-        mDummyView = getDummyView();
-        mView = mDummyView;
+        mView = null;
     }
 
-    private MainView getDummyView() {
-        return new MainView() {
-            @Override
-            public void showProgress() {
-                Log.d(LOG_TAG, "Dummy show progress");
-            }
-
-            @Override
-            public void hideProgress() {
-                Log.d(LOG_TAG, "Dummy hide progress");
-            }
-
-            @Override
-            public void setImage(Bitmap image) {
-                Log.d(LOG_TAG, "Dummy set image");
-            }
-
-            @Override
-            public int getImageHeight() {
-                Log.d(LOG_TAG, "Dummy get height");
-                return 0;
-            }
-
-            @Override
-            public int getImageWidth() {
-                Log.d(LOG_TAG, "Dummy get width");
-                return 0;
-            }
-
-            @Override
-            public void showToast(String s) {
-                Log.d(LOG_TAG, "Dummy show toast");
-            }
-        };
+    private boolean isViewAttached() {
+        return mView == null;
     }
 
     @Override
@@ -75,11 +40,12 @@ public class MainPresenterImpl implements MainPresenter {
 
     @Override
     public void detachView() {
-        mView = mDummyView;
+        mView = null;
     }
 
     @Override
     public void loadCurrentImage() {
+        mView.showProgress();
         mDataManager.getBitmapFromSdoObservable()
                 .observeOn(Schedulers.computation())
                 .flatMap(bmp -> {
